1de5b22b13e9d02af0bcd1b5db5db71057af2c5b
Sergey Korolyov <56508293+SergeiKorolyov@users.noreply.github.com>
2019-10-17 19:46:11 +0300

    Add files via upload

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -15,58 +15,49 @@ public class Main {
 
     public static void main(String[] args) {
 //1 способ int to string//
-        print(String.valueOf(inttostring(4123)));
+        System.out.println(inttostring(4123));
 //2 способ int to string//
-        print(String.valueOf(Integer.toString(5654)));
+        System.out.println(Integer.toString(5654));
 //1 способ string to int//
-        int x = Integer.valueOf(stringtoint("1234"));
+        int x = Integer.valueOf("1234");
         int s = x + 5;
         System.out.println(s);
 //2 способ string to int//
-        int y = Integer.parseInt(stringtoint("345"));
+        int y = Integer.parseInt("345");
         int d = y + 3;
         System.out.println(d);
 //четность числа//
-        System.out.println(evennumber(4));
+        System.out.println(evenNumber(4));
 ////окончание на 7//
-        System.out.println(number7(1787));
+        System.out.println(endWith7(1787));
 //определение символа в строке//
-        System.out.println(symbol("you are happy"));
+        System.out.println(symbol("how are you", 'z'));
 //часы в минуты и секунды//
-        print(minsec(3));
+        System.out.println(hoursToMinAndSec(3));
 //секунды в дни, часы, минут и секунды//
-        print(dayshoursmin(45089));
+        System.out.println(secondsToDaysHoursMinSeconds(45089));
 // изменение регистра //
-        print(chardance("asdfgghjg"));
+        System.out.println(charDance("asdfgghjg"));
 //вычисление площади и длины круга//
-        print(circlepar(45.1));
+        System.out.println(circlePar(45.1));
 // закрывает ли круг отверстие//
         System.out.println(hole(-5, 5, 6));
 //вычисление возраста (работает плохо, на все месяцы имеющие номер больше текущего выдает отрицательные значения, не правильно показывает день)//
-        System.out.println(birthday2(1995,1,18));
+        System.out.println(birthday(1995, 1, 18));
     }
 
-
-    public static void print(String s) {
-        System.out.println(s);
-
-    }
 
     public static int inttostring(int a) {
         return a;
 
     }
 
-    public static String stringtoint(String b) {
 
-        return b;
-    }
-
-    public static boolean evennumber(int a) {
+    public static boolean evenNumber(int a) {
         return (a % 2 == 0);
     }
 
-    public static boolean number7(int number) {
+    public static boolean endWith7(int number) {
         if (number % 10 != 7) {
             return false;
         } else {
@@ -74,20 +65,23 @@ public class Main {
         }
     }
 
-    public static boolean symbol(String a) {
-        boolean contein = stringtoint(a).contains("r");
-        return contein;
-
+    public static boolean symbol(String a, char c) {
+        if (a.indexOf(c) != -1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public static String minsec(int hours) {
+
+    public static String hoursToMinAndSec(int hours) {
         int minutes = hours * 60;
         int seconds = hours * 3600;
         String res = minutes + " минут " + seconds + " секунд";
         return res;
     }
 
-    public static String dayshoursmin(int sec) {
+    public static String secondsToDaysHoursMinSeconds(int sec) {
         int day = (int) TimeUnit.SECONDS.toDays(sec);
         long hours = TimeUnit.SECONDS.toHours(sec) - (day * 24);
         long minute = TimeUnit.SECONDS.toMinutes(sec) - (TimeUnit.SECONDS.toHours(sec) * 60);
@@ -96,8 +90,7 @@ public class Main {
         return res;
     }
 
-    // НЕПОНЯТНО //
-    public static String chardance(String s) {
+    public static String charDance(String s) {
         char[] chars = s.toCharArray();
         String rez = "";
         for (int i = 0; i < chars.length; i++) {
@@ -113,43 +106,46 @@ public class Main {
 
     }
 
-    public static String circlepar(double r) {
-        double square = 3.14 * r * r;
-        double lenght = 2 * 3.14 * r;
+    public static String circlePar(double r) {
+        double square = Math.PI * r * r;
+        double lenght = 2 * Math.PI * r;
         String result = square + " Площадь окружности " + lenght + " Длина окружности";
         return result;
     }
 
     public static boolean hole(double r, double a, double b) {
-        double circlesquare = 3.14 * r * r;
+        double circleSquare = Math.PI * r * r;
         double hole = a * b;
-        if (hole < circlesquare && hole>0){
+        if (hole < circleSquare && hole > 0 && r > 0) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public static String birthday2 (int year, int month, int day){
-    Date now = new Date();
-    int nowMonth = now.getMonth()+1;
-    int nowYear = now.getYear()+1900;
-    int nowDay1  = now.getDay() - day;
-    int resultYear = nowYear - year;
-    int resultMonth = nowMonth - month;
-    String result = "" + resultYear + " years " + resultMonth + " month " + nowDay1 + " days";
+    public static String birthday(int year, int month, int day) {
+        LocalDate bday = LocalDate.of(year, month, day);
+        Period bdayrezult = Period.between(bday, LocalDate.now());
+        return "You are " + bdayrezult.getYears() + " years " + bdayrezult.getMonths() + " months " + bdayrezult.getDays() + " days";
+        //LocalDate date = LocalDate.now();
+        //int Month = date.getMonthValue();
+        //int Year = date.getYear();
+        //int Day = date.getDayOfMonth();
+        //int bdYear = Year - year;
+        //int bdMonth = Month - month;
+        //int bdDay = Day - day;
+        //String result = "" + bdYear + " years " + bdMonth + " month " + bdDay + " days";
 
-    if (month > nowMonth) {
-        resultYear --;
-    }
-    else if (month > nowMonth){
-        int nowDay = now.getDate();
+        //if (bdMonth > month) {
+          //  bdMonth++;
+        //} else if (month > Month) {
 
-        if (day > nowDay)  {
-            resultMonth--;
-        }
+          //  if (bdDay > Day) {
+            //    bdMonth++;
+            //}
+        //}
+        //return result;
     }
-    return result;}
 }
 
 
